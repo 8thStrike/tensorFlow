2a2b02130a044a29b8a08def74cdbff73eba0256
8thStrike <edouard7@gmail.com>
2017-12-01 12:59:46 +0100

    finally mastering ndarray filling...

--- aa.py
+++ aa.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+'''
 W = tf.Variable(tf.zeros([7,1]))
 print(W)
 
@@ -8,9 +9,65 @@ sess=tf.InteractiveSession()
 print(tf.transpose(tf.zeros([7])).eval(session=sess))
 
 print(tf.zeros([1,7]).eval(session=sess))
+
 print(np.transpose(tf.zeros([7,1]).eval(session=sess)))
 
 
-test=np.array([[1],[7]])
+#This is how you add vector to a vector of vector (or line to a matrix)
+test=np.array([[1,45,4,34,23],[5,4,56,111,7]])
 
 print(test)
+
+test=np.append(test,[[1,2,3,56,987]], 0)
+
+print(test)
+
+
+'''
+
+
+
+#Other trial
+print("Other Trial with append on axis 1")
+test=np.array([[]], dtype=float)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],1) #Obligatory : axe 1 for the first vector
+print(test)
+
+test=np.append(test, [[1,2,3,4,5]],0)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],0)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],0)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],0)
+print(test)
+
+#array method
+
+print(test.lenght)
+'''
+test=np.append(test, [[1,2,3,4,5]],1)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],1)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],1)
+print(test)
+test=np.append(test, [[1,2,3,4,5]],1)
+print(test)
+
+#other trial
+print("Other Trial with append on axis 0")
+test=np.array([[]])
+print(test)
+test=np.append(test, [[1,2,3,4,5]])
+print(test)
+test=np.append(test, [[1,2,3,4,5]])
+print(test)
+test=np.append(test, [[1,2,3,4,5]])
+print(test)
+test=np.append(test, [[1,2,3,4,5]])
+print(test)
+test=np.append(test, [[1,2,3,4,5]])
+print(test)
+'''
--- spreadMultifactor.py
+++ spreadMultifactor.py
@@ -14,9 +14,11 @@ col1, col2, col3, col4, col5, col6, col7, col8, col9  = tf.decode_csv(
 market = tf.stack([col2, col3, col5, col6, col7, col8, col9])    #col4 is DEM 10y, the Variable to explain
 #usa = tf.stack([col6, col7, col8, col9])
 tenDEM = tf.stack([col4])
-market_train=np.array([])
 
-
+market_train_array=np.array([])
+market_test_array=np.array([])
+tenDEM_test=np.array([])
+tenDEM_train=np.array([])
 
 
 with tf.Session() as sess:
@@ -26,23 +28,32 @@ with tf.Session() as sess:
 
   for i in range(1800):
     # Retrieve a single instance:
-    date_train, market_train, tenDEM_train = sess.run([col1,market, tenDEM]) #, col3, col4, col5, col6, col7, col8, col9])
+    date_train, market_train, tenDEM_temp = sess.run([col1,market, tenDEM]) #, col3, col4, col5, col6, col7, col8, col9])
+    market_train_array=np.append(market_train_array, market_train,0)
+    tenDEM_train=np.append(tenDEM_train, tenDEM_temp,0)
+
   for i in range(1801,2100):
-    date_test, market_test, tenDEM_test = sess.run([col1, market, tenDEM])
+    date_test, market_test, tenDEM_temp = sess.run([col1, market, tenDEM])
+    market_test_array=np.append(market_test_array,market_train,0 )
+    tendDEM_test=np.append(tenDEM_test, tenDEM_temp,0)
+
   coord.request_stop()
   coord.join(threads)
 
+print(tenDEM_train)
+
 
 sess=tf.InteractiveSession()
+
 # Model parameters
-
 W = tf.Variable(tf.zeros([7,1]))
-
-
 b = tf.Variable([0.0], dtype=tf.float32)
+
+
 # Model input and output
 x = tf.placeholder(tf.float32, shape=[None, 7])
 y = tf.nn.softmax(tf.matmul(x, W) + b)
+
 
 #training
 y_ = tf.placeholder(tf.float32)
@@ -52,12 +63,10 @@ train = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 sess= tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-print(market_train)
-print(market_train.T)
 
 for i in range(200):
-    x_train = market_train.T
-    y_train = tf.constant(tenDEM_train).eval(session=sess)
+    x_train = market_train_array
+    y_train = tenDEM_train
     sess.run(train, {x: x_train, y: y_train})
 
 # evaluate training accuracy
